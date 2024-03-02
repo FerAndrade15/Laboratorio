942ca843d3a375b62e9fbf5e8b56e4fbcd78c396
FerAndrade15 <and22676@uvg.edu.gt>
2024-03-02 16:58:29 -0600

    Timer0 / initial settings

--- Proyecto1/Proyecto1/main.asm
+++ Proyecto1/Proyecto1/main.asm
@@ -1,4 +1,4 @@
-//********************************************
+//*******************************************************************
 // Universidad del Valle de Guatemala
 // IE2023: Programación de microcontroladores
 // Autor: María Andrade
@@ -6,11 +6,11 @@
 // Archivo: Proyecto1.asm
 // Hardware: ATMEGA328P
 // Created: 21/02/2024 8:06:46
-//********************************************
+//*******************************************************************
 // Encabezado -------------------------------------------------------
 .include "M328PDEF.inc"
-
 .dseg
+//Settings registers ------------------------------------------------
 .org SRAM_START
 	settings_hours:		.byte 1
 	settings_minutes:	.byte 1
@@ -18,15 +18,12 @@
 	settings_days:		.byte 1
 	settings_month:		.byte 1
 	settings_years:		.byte 1
-
 .cseg 
 // Interrupt Vectors ------------------------------------------------
 .org 0x00					;Reset
 	JMP START
 .org 0x001C					;Compare Match A
-	JMP START
-	//JMP TIMER0_COMPA
-
+	JMP TIMER0_COMPA
 // Reset --------------------------------------------------------- //
 START:
 // Stack Pointer ----------------------------------------------------
@@ -34,44 +31,74 @@ LDI R16, LOW(RAMEND)
 OUT SPL, R16
 LDI R17, HIGH(RAMEND)
 OUT SPH, R17
-
 //Functional Registers ----------------------------------------------
 .def hours	= R18
 .def minutes= R19
 .def seconds= R20
-.def ten_ms	= R21
+.def five_ms= R21
 .def day	= R22
 .def month	= R23
 .def year	= R24
-
+.def delmode= R25
+.def btn_del= R26
 //General Settings --------------------------------------------------
 Setup:
+	//Timer0 Settings
 	LDI R16, 0x05
 	OUT TCCR0B, R16				;Clock selected >> Prescaler at 1024
 	LDI R16, 0x02	
 	OUT TCCR0A, R16				;Timer mode >> CTC Mode
 	STS TIMSK0, R16				;Enable Interrupt Mask for Compare Match A
-	LDI R16, 0x9C
+	LDI R16, 0x4E
 	OUT OCR0A, R16				;Max for CTC Mode >> Generates interrrupt
-	SEI							;Enable all interruptions
-
+	//Pinchange Settings
+	LDI R16, 1
+	STS PCICR, R16				;Enable Pin Change Interrupt on PINB 
+	LDI R16, 28	
+	STS PCMSK0, R16				;Enable PB2, PB3 & PB4 on Pin Change Mask Register
 	//Setting initial values
-	LDI ten_ms, 99
+	LDI five_ms, 199
 	LDI seconds, 59
 	LDI minutes, 59
 	LDI hours, 23
-	LDI day, 1
-	LDI month, 2
+	LDI day, 30
+	LDI month, 4
 	LDI year, 24
-
+	CLR mode
+	//Enable all interruptions
+	SEI
 //Main loop ---------------------------------------------------------
 Loop:
+	//5ms starter
+	SBRS delmode, 7				;confirms 5ms delay to change display
+	JMP Loop
+continue_diplay:					
+	MOV R16, delmode			;copy register display selector
+	SWAP R16
+	CBR R16, 0xF8				;3 bits of display selector				
+complete_loop:
+	JMP Loop
 // CTC ----------------------------------------------------------- //
-//TIMER0_COMPA:	
-	INC ten_ms
-	CPI ten_ms, 100				;ten_ms equals 1s
+TIMER0_COMPA:	
+	PUSH R16					;save registers and sreg from main loop
+	PUSH R17
+	IN R16, SREG
+	PUSH R16
+	INC five_ms
+	STS R16, PCMSK0				;PCMSK0 clear after pressing buttons
+	CPI R16, 0
+	BRNE continue_timer
+	INC btn_del					;20ms counter
+	CPI btn_del, 3
+	BRNE continue_timer
+	CLR btn_del					;reset buttons delay
+	LDI R16, 28	
+	STS PCMSK0, R16				;Enable PB2, PB3 & PB4 on Pin Change Mask Register
+continue_timer:
+	SBR delmode, 0x80			;b1xxx_xxxx to show displays
+	CPI five_ms, 200			;five_ms equals 1s
 	BRNE completeinterrupt
-	CLR ten_ms					;restart ten_ms
+	CLR five_ms					;restart five_ms
 	INC seconds
 	CPI seconds, 60				;seconds equals 1m
 	BRNE completeinterrupt
@@ -87,27 +114,32 @@ Loop:
 	LDI ZH, HIGH(days_month<<1)	;redirect to days of month table
 	LDI ZL, LOW(days_month<<1)
 	ADD ZL, month				;correct to the actual month
-	LD R16, Z					;max day of month
+	LPM R16, Z					;max day of month
 	CPI month, 2
 	BRNE checkmaxofmonth
 	MOV R17, year
-	CBR R17, 0xFC				;conserve less 2 significant bits
+	CBR R17, 0xFC				;conserve less 2 significant bits	
 	CPI R17, 0					;check if the number is multiple of four
 	BRNE checkmaxofmonth
 	LDI R16, 29					;max of february/leap_year
 checkmaxofmonth:
-	CP R16, day
-	BREQ incrementmonth
-	INC day
+	CP R16, day					;check if the actual day is the max of the month
+	BREQ incrementmonth			;change of month
+	INC day		
 	JMP completeinterrupt
-
+incrementmonth:
+	LDI day, 1					;start of month
+	INC month
+	CPI month, 13				;restart of the year
+	BRNE completeinterrupt
+	LDI month, 1
+	INC year					;increment years
 completeinterrupt:
-	SBRC ten_ms, PB0
-	JMP Loop
-	//Display
-	JMP Loop
-	//RETI
+	POP R16						;restore registers and sreg from main loop
+	OUT SREG, R16
+	POP R17
+	POP R16
+	RETI
+//Info Tables -------------------------------------------------------
 .org 0x100
     days_month:			.DB 0,31,28,31,30,31,30,31,31,30,31,30,31,0			;14 bytes
-	//ncode_numbers:		.DB 252,96,218,242,102,182,190,224,254,246			;10 bytes
-	//ucode_numbers:		.DB 252,12,218,158,46,246,28,254,190				;10 bytes
